028a8396916e6bf9de3b81b968b12a96f24bca1d
smilesnake <13077908485@163.com>
2019-03-16 11:51:43 +0800

    启动类
    spring boot 启动类

---
+++ src/main/java/github/com/smilesnake/Application.java
@@ -0,0 +1,13 @@
+package github.com.smilesnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
